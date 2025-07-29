45704fea2dd2a8119ee8f327a6afd1979c3a1d1a
EpicVN <ledangkhoahi3@gmail.com>
2025-07-29 17:18:55 +0700

    chore(seed): add hashing password service and seed data script (roles and admin user)

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -22,6 +22,10 @@ const configSchema = z.object({
   REFRESH_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_EXPIRES_IN: z.string(),
   SECRET_API_KEY: z.string(),
+  ADMIN_NAME: z.string(),
+  ADMIN_PASSWORD: z.string(),
+  ADMIN_EMAIL: z.string(),
+  ADMIN_PHONE_NUMBER: z.string(),
 })
 
 const configServer = configSchema.safeParse(process.env)
@@ -32,6 +36,6 @@ if (!configServer.success) {
   process.exit(1)
 }
 
-const envConfig = configServer
+const envConfig = configServer.data
 
 export default envConfig
