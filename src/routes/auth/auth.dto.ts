b440dad29406f0f0b28357c1591461c44636a62b
EpicVN <ledangkhoahi3@gmail.com>
2025-08-01 14:57:08 +0700

    refactor(auth): apply Repository Pattern for user register

--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,49 +1,7 @@
-import { UserStatus } from '@prisma/client'
 import { createZodDto } from 'nestjs-zod'
-import { z } from 'zod'
 
-const UserSchema = z
-  .object({
-    id: z.number(),
-    email: z.string(),
-    name: z.string(),
-    phoneNumber: z.string(),
-    avatar: z.string().nullable(),
-    status: z.enum([UserStatus.ACTIVE, UserStatus.INACTIVE, UserStatus.BLOCKED]),
-    roleId: z.number(),
-    createdById: z.number().nullable(),
-    updatedById: z.number().nullable(),
-    deletedAt: z.date().nullable(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
-  })
-  .strict()
-
-const RegisterBodySchema = z
-  .object({
-    email: z.email(),
-    password: z
-      .string()
-      .min(6, 'Password must be at least 6 characters long')
-      .max(20, 'Password must not exceed 20 characters'),
-    confirmPassword: z
-      .string()
-      .min(6, 'Confirm Password must be at least 6 characters long')
-      .max(20, 'Confirm Password must not exceed 20 characters'),
-    name: z.string().min(1).max(50),
-    phoneNumber: z.string().min(9).max(15),
-  })
-  .strict()
-  .superRefine(({ password, confirmPassword }, ctx) => {
-    if (password !== confirmPassword) {
-      ctx.addIssue({
-        code: 'custom',
-        message: 'Password and confirm password do not match',
-        path: [confirmPassword],
-      })
-    }
-  })
+import { RegisterBodySchema, RegisterResSchema } from './auth.model'
 
 export class RegisterBodyDTO extends createZodDto(RegisterBodySchema) {}
 
-export class RegisterResDTO extends createZodDto(UserSchema) {}
+export class RegisterResDTO extends createZodDto(RegisterResSchema) {}
--- src/routes/auth/auth.module.ts
+++ src/routes/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { RolesService } from './roles.service';
 import { TokenService } from 'src/shared/services/token.service';
+import { AuthRepository } from './auth.repo';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, RolesService, TokenService]
+  providers: [AuthService, RolesService, TokenService, AuthRepository]
 })
 export class AuthModule {}
